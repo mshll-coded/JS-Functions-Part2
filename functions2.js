d69bc095467bf9571ffd470548f1b5d4ce8457bf
meshal <imeshalfm@gmail.com>
2024-10-07 11:23:13 +0300

    complete task

---
+++ functions2.js
@@ -0,0 +1,47 @@
+/**
+ * isOdd(n):
+ * - receives a number n
+ * - returns true if it's odd, false otherwise
+ *
+ * e.g.
+ * isOdd(7) -> true
+ * isOdd(10) -> false
+ */
+function isOdd(n) {
+  return !!(n % 2);
+}
+console.log(`isOdd(7) -> ${isOdd(7)}`);
+console.log(`isOdd(10) -> ${isOdd(10)}`);
+
+/**
+ * oddsSmallerThan(n):
+ * - receives a number n
+ * - returns the number of ODD numbers smaller than n
+ *
+ * e.g.
+ * oddsSmallerThan(7) -> 3
+ * oddsSmallerThan(15) -> 7
+ */
+function oddsSmallerThan(n) {
+  let res = 0;
+  for (let i = 0; i < n; i++) res += i % 2;
+  return res;
+}
+console.log(`oddsSmallerThan(7) -> ${oddsSmallerThan(7)}`);
+console.log(`oddsSmallerThan(15) -> ${oddsSmallerThan(15)}`);
+
+/**
+ * squareOrDouble(n):
+ * - receives a number n
+ * - returns its square if it's odd
+ * - returns its double if it's even
+ *
+ * e.g.
+ * squareOrDouble(16) -> 32
+ * squareOrDouble(9) -> 81
+ */
+function squareOrDouble(n) {
+  return n % 2 ? n ** 2 : n * 2;
+}
+console.log(`squareOrDouble(16) -> ${squareOrDouble(16)}`);
+console.log(`squareOrDouble(9) -> ${squareOrDouble(9)}`);
